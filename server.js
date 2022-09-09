9633cdadabb993b7fe0071228593ffd9a9f6deb6
colinmichael89 <colinmichael89@gmail.com>
2022-09-09 17:24:14 -0600

    update

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Get Route for homepage
-app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/public")));
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "public")));
 
 // Get Route for notes page
 app.get("/notes", (req, res) =>
@@ -22,19 +22,19 @@ app.get("/notes", (req, res) =>
 
 // Get Route for saved notes
 app.get("/api/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/db/db.json"));
+  res.sendFile(path.join(__dirname, "db/db.json"));
 });
 
 // Post new note
 app.post("/api/notes", (req, res) => {
-  const notes = JSON.parse(fs.readFileSync("/db/db.json"));
+  const notes = JSON.parse(fs.readFileSync("db/db.json"));
   const newNote = req.body;
   console.info(`\nExecuting ${req.method} of New Note`);
   // Create uuid
   newNote.id = uuid.v4();
 
   notes.push(newNote);
-  fs.writeFileSync("/db/db.json", JSON.stringify(notes));
+  fs.writeFileSync("db/db.json", JSON.stringify(notes));
   console.info("\nNew note successfully added!");
   res.json(notes);
 });
@@ -42,11 +42,11 @@ app.post("/api/notes", (req, res) => {
 // Delete note by ID
 app.delete("/api/notes/:id", (req, res) => {
   let noteId = req.params.id;
-  const notes = JSON.parse(fs.readFileSync("/db/db.json"));
+  const notes = JSON.parse(fs.readFileSync("db/db.json"));
   const deleteNoteEl = notes.filter(
     (removeNote) => removeNote.id !== req.params.id
   );
-  fs.writeFileSync("/db/db.json", JSON.stringify(deleteNoteEl));
+  fs.writeFileSync("db/db.json", JSON.stringify(deleteNoteEl));
   console.info(`\nSuccessfully deleted note with id : ${req.params.id}`);
   res.json(deleteNoteEl);
 });
